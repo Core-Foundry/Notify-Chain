5ccaf20ae133c5b256e581436b89c187c2d55476
nikkybel <sulymanaishat0@gmail.com>
2026-08-29 15:04:36 +0100

    feat:implement Protect Discord Notifications Against Message-Length Limits

--- listener/jest.config.js
+++ listener/jest.config.js
@@ -1,20 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { diagnostics: false }]
-  },
-  moduleNameMapper: {
-    '^uuid$': '<rootDir>/src/__mocks__/uuid.js'
     '^.+\\.tsx?$': ['ts-jest', {
-      diagnostics: {
-        ignoreCodes: [2307]
-      }
+      diagnostics: false,
     }]
   },
   moduleNameMapper: {
+    '^uuid$': '<rootDir>/src/__mocks__/uuid.js',
     '^@stellar/stellar-sdk$': '<rootDir>/src/__mocks__/@stellar/stellar-sdk.ts',
     '^node-cache$': '<rootDir>/src/__mocks__/node-cache.ts'
   },
